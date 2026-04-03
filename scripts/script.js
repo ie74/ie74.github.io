eea8dad4d6050a985b7d3e6fe1f31f290335b1e6
IE74 <146759080+IE74@users.noreply.github.com>
2026-04-03 21:39:00 +0200

    Populated gallery page. 	new file:   css/photography.css 	new file:   json/photos.json 	modified:   pages/photography.html 	modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
+// Header
 const headerMenu = document.querySelector(".header-links");
 const headerMenuBtn = document.querySelector(".header-menu-btn");
 const headerMenuBtnIcon = document.querySelector(".header-menu-btn i");
 
+// Photos
+const gallery = document.querySelector(".section#gallery .section-container");
+
+// Responsive Header logic
 headerMenuBtn.addEventListener("click", () => {
     if(headerMenu.classList.contains("active")){
         headerMenu.classList.remove("active");
@@ -13,3 +18,15 @@ headerMenuBtn.addEventListener("click", () => {
         headerMenuBtnIcon.classList.add("bi-x-lg");
     }
 });
+
+//Load photos
+fetch("../json/photos.json").then(response => {
+    return response.json();
+}).then(data => {
+    data.forEach(photo => {
+        gallery.innerHTML += `<div class="img-container">
+							        <img src="../${photo.path}" alt="image" class="gallery-img">
+							        <p class="img-desc">${photo.desc}</p>
+						        </div>`
+    });
+})
