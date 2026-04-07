48da9c7d6d31f565bf2ee36f84f305f58ca3722f
IE74 <146759080+IE74@users.noreply.github.com>
2026-04-07 21:12:13 +0200

    deleted:    json/photos.json 	modified:   pages/photography.html 	modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -18,15 +18,3 @@ headerMenuBtn.addEventListener("click", () => {
         headerMenuBtnIcon.classList.add("bi-x-lg");
     }
 });
-
-//Load photos
-fetch("../json/photos.json").then(response => {
-    return response.json();
-}).then(data => {
-    data.forEach(photo => {
-        gallery.innerHTML += `<div class="img-container">
-							        <img src="../${photo.path}" alt="image" class="gallery-img">
-							        <p class="img-desc">${photo.desc}</p>
-						        </div>`
-    });
-})
